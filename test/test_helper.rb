f2c1ee5946092ad51ecdc2ae447625f609e28b3c
Matthew Yang <yang70@gmail.com>
2015-09-08 15:09:51 -0700

    small modifications

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,7 +6,7 @@ require "minitest/rails/capybara"
 require "minitest/pride"
 require "minitest/reporters"
 
-Minitest::Reporters.use!
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 class ActiveSupport::TestCase
   fixtures :all
