bfc87a8e820b94738509e9d0716350eb021f3acb
Matthew Yang <yang70@gmail.com>
2015-09-15 14:35:09 -0700

    tests passing for twitter login

--- test/features/auth/sign_in_out_test.rb
+++ test/features/auth/sign_in_out_test.rb
@@ -18,11 +18,11 @@ feature "Sign In and Out" do
                               uid: '12345',
                               info: { nickname: 'test_twitter_user' },
                               })
-    visit root_path
+    visit new_user_session_path
     Capybara.current_session.driver.request.env['devise.mapping'] = Devise.mappings[:user]
     Capybara.current_session.driver.request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:twitter]
 
     click_on "Sign in with Twitter"
-    page.must_have_content "Logged in as test_twitter_user"
+    page.must_have_content "test_twitter_user, you are signed in!"
   end
 end
