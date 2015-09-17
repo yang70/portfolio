8cf190b4116a2e7559b3e01282358ec7d2806a54
Matthew Yang <yang70@gmail.com>
2015-09-17 10:04:50 -0700

    fix oauth

--- test/functional/prevent_hacking_url_test.rb
+++ test/functional/prevent_hacking_url_test.rb
@@ -1,9 +1,7 @@
 # require "test_helper"
 
-# feature "Authors can't hack URL to publish post or change role" do
-#   scenario "Author cannot post directly to create with publish true" do
-#     sign_in(:author)
+# class ArticlesControllerTest < ActionController::TestCase
+#   test "Author cannot post directly to create with publish true" do
 #     post :create, articles: {title: "Direct post", body: "test body"}
-#     save_and_open_page
 #   end
 # end
