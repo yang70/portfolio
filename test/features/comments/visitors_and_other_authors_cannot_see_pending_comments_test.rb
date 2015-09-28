2300f756c5c185e1235d585335f484a5cbdf18be
Matthew Yang <yang70@gmail.com>
2015-09-27 22:54:04 -0700

    update comment view permissions test

--- test/features/comments/visitors_and_other_authors_cannot_see_pending_comments_test.rb
+++ test/features/comments/visitors_and_other_authors_cannot_see_pending_comments_test.rb
@@ -1,15 +1,21 @@
 require 'test_helper'
 
 feature "non logged in visitor and other authors cannot see pending commments" do
+
+  scenario "verify comment there for editor" do
+    sign_in(:editor)
+    visit article_path(articles(:post4))
+    page.text.must_include "Test comment"
+  end
+
   scenario "non logged in visitors cannot see pending comments" do
-    visit article_path(articles(:post1))
+    visit article_path(articles(:post4))
     page.text.wont_include "Test comment"
   end
 
   scenario "other authors cannot see pending comments" do
     sign_in(:author)
-    visit article_path(articles(:post2))
-    page.text.wont_include "Editor test comment"
-    page.text.wont_include "Pending"
+    visit article_path(articles(:post4))
+    page.text.wont_include "Test comment"
   end
 end
