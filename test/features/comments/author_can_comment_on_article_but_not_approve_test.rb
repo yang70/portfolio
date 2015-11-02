f10265ae05bab506d1d972a924c33b93510ac8c6
Matthew Yang <yang70@gmail.com>
2015-11-02 06:43:07 -0800

    comment out comment tests until comments enabled again

--- test/features/comments/author_can_comment_on_article_but_not_approve_test.rb
+++ test/features/comments/author_can_comment_on_article_but_not_approve_test.rb
@@ -1,12 +1,12 @@
-require 'test_helper'
+# require 'test_helper'
 
-feature "author can comment on article but not approve the comment" do
-  scenario "log in as author, visit an article and leave comment" do
-    sign_in(:author)
-    visit article_path(articles(:post1))
-    fill_in 'Enter comment below', with: "I'm commenting!"
-    click_on 'Create Comment'
-    page.text.must_include "Comment submitted, must be approved by an editor."
-    page.text.must_include "Comment invisible until approved by an editor"
-  end
-end
+# feature "author can comment on article but not approve the comment" do
+#   scenario "log in as author, visit an article and leave comment" do
+#     sign_in(:author)
+#     visit article_path(articles(:post1))
+#     fill_in 'Enter comment below', with: "I'm commenting!"
+#     click_on 'Create Comment'
+#     page.text.must_include "Comment submitted, must be approved by an editor."
+#     page.text.must_include "Comment invisible until approved by an editor"
+#   end
+# end
--- test/features/comments/visitors_and_other_authors_cannot_see_pending_comments_test.rb
+++ test/features/comments/visitors_and_other_authors_cannot_see_pending_comments_test.rb
@@ -1,20 +1,20 @@
-require 'test_helper'
+# require 'test_helper'
 
-feature "non logged in visitor and other authors cannot see pending commments" do
-  scenario "verify comment there for editor" do
-    sign_in(:editor)
-    visit article_path(articles(:post4))
-    page.text.must_include "Test comment"
-  end
+# feature "non logged in visitor and other authors cannot see pending commments" do
+#   scenario "verify comment there for editor" do
+#     sign_in(:editor)
+#     visit article_path(articles(:post4))
+#     page.text.must_include "Test comment"
+#   end
 
-  scenario "non logged in visitors cannot see pending comments" do
-    visit article_path(articles(:post4))
-    page.text.wont_include "Test comment"
-  end
+#   scenario "non logged in visitors cannot see pending comments" do
+#     visit article_path(articles(:post4))
+#     page.text.wont_include "Test comment"
+#   end
 
-  scenario "other authors cannot see pending comments" do
-    sign_in(:author)
-    visit article_path(articles(:post4))
-    page.text.wont_include "Test comment"
-  end
-end
+#   scenario "other authors cannot see pending comments" do
+#     sign_in(:author)
+#     visit article_path(articles(:post4))
+#     page.text.wont_include "Test comment"
+#   end
+# end
