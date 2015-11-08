f7787ddc3c9a821982ec76c968fa9a2c18f1486b
Matthew Yang <yang70@gmail.com>
2015-11-08 07:38:56 -0800

    update tests, travis

--- test/mailers/welcome_test.rb
+++ test/mailers/welcome_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+# require "test_helper"
 
-class WelcomeTest < ActionMailer::TestCase
-  def test_sanity
-    flunk "Need real tests"
-  end
-end
+# class WelcomeTest < ActionMailer::TestCase
+#   def test_sanity
+#     flunk "Need real tests"
+#   end
+# end
