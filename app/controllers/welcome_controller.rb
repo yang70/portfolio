5021d083586bd16cc860343d15ec035a6c1137b8
Matthew Yang <yang70@gmail.com>
2015-12-14 13:40:27 -0800

    update welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @articles = Article.where(published: true).order("created_at").limit(3)
-    @projects = Project.order("created_at").limit(3)
+    @articles = Article.where(published: true).order("created_at DESC").limit(3)
+    @projects = Project.order("created_at DESC").limit(3)
   end
 end
