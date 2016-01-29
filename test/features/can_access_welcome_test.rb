b0c4e9633df39c4605c4b2080316b78d5b3af327
Matt Yang <yang70@gmail.com>
2016-01-29 14:23:46 -0800

    fix tests

--- test/features/can_access_welcome_test.rb
+++ test/features/can_access_welcome_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 feature "CanAccessWelcome" do
   scenario "is displaying correct content" do
     visit root_path
-    page.must_have_content "About"
-    page.must_have_content "Recent Blog Posts"
+    page.must_have_content "Blog"
+    page.must_have_content "Projects"
   end
 end
